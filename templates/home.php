dce545a2ff669e770475ae202bf487898328f9c3
RockyLatchman <r.latchman3624@gmail.com>
2025-10-25 21:54:02 +0700

    HTML for header section of home template

--- templates/home.php
+++ templates/home.php
@@ -1,1 +1,21 @@
-<h1>Home</h1>
+
+<header>
+    <div class="logotype">
+       <h1>Taskr</h1>
+    </div>
+    <nav>
+      <menu>
+          <li><a href="">Browse Jobs</a></li>
+          <li><a href="">Top careers</a></li>
+          <li><a href="">For employers</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Contact us</a></li>
+      </menu>
+    </nav>
+    <nav>
+       <menu>
+          <li><a href="">Log in</a></li>
+          <li><a href="">Sign up</a></li>
+       </menu>
+    </nav>
+</header>
