83feefaebdd3df40d657b8dc530183464409351b
RockyLatchman <r.latchman3624@gmail.com>
2025-10-25 21:45:32 +0700

    URL routing and templates for homepage and 404

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+#ini_set('display_errors', '1');
+#error_reporting(E_ALL);
+$request = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+$path = parse_url($request, PHP_URL_PATH);
+include_once '../templates/header.php';
+
+switch ($path) {
+    case '/taskr/':
+        include_once '../templates/home.php';
+        break;
+
+    default:
+        http_response_code(404);
+        include_once '../templates/404.php';
+        break;
+
+}
+
+include_once '../templates/footer.php';
