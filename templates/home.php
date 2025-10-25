8c5ced3f912b63ee34186bc7d8fb9eca966f05ab
RockyLatchman <r.latchman3624@gmail.com>
2025-10-25 23:27:25 +0700

    HTML for home templates

--- templates/home.php
+++ templates/home.php
@@ -5,17 +5,89 @@
     </div>
     <nav>
       <menu>
-          <li><a href="">Browse Jobs</a></li>
-          <li><a href="">Top careers</a></li>
-          <li><a href="">For employers</a></li>
-          <li><a href="">About</a></li>
-          <li><a href="">Contact us</a></li>
+          <li><a href="http://localhost/taskr/browse">Browse Jobs</a></li>
+          <li><a href="http://localhost/taskr/top-careers">Top careers</a></li>
+          <li><a href="http://localhost/taskr/employers">For employers</a></li>
+          <li><a href="http://localhost/taskr/about">About</a></li>
+          <li><a href="http://localhost/taskr/contact">Contact us</a></li>
       </menu>
     </nav>
     <nav>
        <menu>
-          <li><a href="">Log in</a></li>
-          <li><a href="">Sign up</a></li>
+          <li><a href="http://localhost/taskr/signin">Log in</a></li>
+          <li><a href="http://localhost/taskr/register">Sign up</a></li>
        </menu>
     </nav>
 </header>
+<main>
+    <section>
+         <h1>Find your dream job </h1>
+         <p>We are the premier platform connecting top talent with innovative companies</p>
+         <ul>
+           <li><a href="http://localhost/taskr/employer">I’m looking to hire</a></li>
+           <li><a href="http://localhost/taskr/candidates">I’m looking for work</a></li>
+         </ul>
+    </section>
+    <section>
+        <h2>Find a Job You'll Love</h2>
+        <p>
+            Stop the endless scrolling. At Taskr, we believe your career should be a  journey of  growth
+            and  fulfillment. Whether you're taking the next step, changing lanes, or starting fresh, we
+            connect you with opportunities that  match your skills, ambitions, and values
+        </p>
+        <a href="">Create Your Free Profile</a>
+    </section>
+    <section>
+        <h2>Why Job Seekers Choose Us?</h2>
+        <div class="row-container">
+         <div class="row-item">
+            <p>
+                Smart Matches: Our intelligent algorithm learns what you're looking
+                for and surfaces the most relevant jobs, saving you time and effort.
+            </p>
+            <p>
+                Quality Over Quantity: We partner with reputable companies who
+                are genuinely hiring, so you can apply with confidence.
+            </p>
+         </div>
+         <div class="row-item">
+            <p>
+               Your Career Hub: Go beyond the search. Access expert resources,
+               resume tips, and interview advice to help you succeed at every stage.
+            </p>
+            <p>
+                Total Transparency: Get insights into company culture,
+                salary ranges, and benefits before you even apply
+            </p>
+         </div>
+        </div>
+    </section>
+    <section>
+        <h2>Build Your Team. Shape Your Future.</h2>
+        <p>
+            Your company's greatest asset is its people. We help you build the team that will drive
+            your vision forward. From startup disruptors to established industry leaders, we provide
+            the platform and visibility to attract talent that aligns with your mission and culture.
+        </p>
+        <a href="">Learn more for employers</a>
+    </section>
+    <footer>
+        <div><span>By the Numbers         10,000+ Careers Advanced       5,000+ Companies Hired     50,000+ Meaningful Connections Made</span></div>
+        <div id="newsletter">
+            <h4>Sign up for our newsletter</h4>
+            <p>We'll send you a newsletter every Thursday with the latest remote jobs</p>
+            <form method="POST">
+                <input type="email" name="email" placeholder="youremail@example.com" required>
+                <input type="submit" name="submit" value="Subscribe">
+            </form>
+        </div>
+        <div id="company">
+            <h4>Company</h4>
+            <menu>
+               <li><a href="http://localhost/taskr/">About us</a></li>
+               <li><a href="http://localhost/taskr/careers">Careers</a></li>
+               <li><a href="http://localhost/taskr/contact">Contact us</a></li>
+            </menu>
+        </div>
+    </footer>
+</main>
