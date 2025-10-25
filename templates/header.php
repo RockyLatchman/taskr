c49f665d1d0110db22cadb4256006c6e0e28bbee
RockyLatchman <r.latchman3624@gmail.com>
2025-10-25 21:24:46 +0700

    HTML for header template

---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+        <title>Taskr</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="http://localhost/taskr/main.css">
+        <script src="http://localhost/taskr/js/main.js"></script>
+    </head>
+    <body>
