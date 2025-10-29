8c7455593aa0a39c274d43385fbd7e529742a82c
RockyLatchman <r.latchman3624@gmail.com>
2025-10-29 10:41:59 +0700

    CSS for employers template

--- templates/employers.php
+++ templates/employers.php
@@ -5,27 +5,27 @@
     </div>
     <?php require 'includes/menu.php'; ?>
 </header>
+<div id="employers-container">
 <main>
     <section>
        <h1>Employers</h1>
-       <article>
          <h3>Source Quality Candidates, Not Just Resumes</h3>
          <p>
-             $Your time is valuable. Stop sifting through unqualified applications and start connecting with candidates who are  ready to make an impact.
+             Your time is valuable. Stop sifting through unqualified applications and start connecting with candidates who are  ready to make an impact.
              Taskr gives you the tools and the targeted audience to streamline your  hiring process  and secure top talent faster.
          </p>
-          <h4>Why Recruiters Trust Us</h4>
+          <h3>Why Recruiters Trust Us</h3>
           <p>
               Your time is valuable. Stop sifting through unqualified applications and start connecting with candidates who are ready to make an impact.
               Taskr gives you the tools and the targeted audience to streamline your hiring process and secure top talent faster.
           </p>
           <ul>
             <li>Reach Active & Passive Talent:Our platform is filled with professionals actively seeking new roles and those open to the right opportunity.</li>
-            <li>Advanced Candidate Filtering: Use our powerful filters for skills, experience, location, and more to pinpoint your ideal candidate in seconds.</li>
-            <li>Streamline Your Workflow: Manage job postings, applications, and communication from one simple, intuitive dashboard.</li>
-            <li>Strengthen Your Employer Brand: Showcase your company culture with rich profile pages, videos, and employee stories to attract the best fits.</li>
+            <li>Advanced Candidate Filtering: Use our powerful filters for skills, experience, location, and more to pinpoint your ideal candidate in seconds.</li>
+            <li>Streamline Your Workflow: Manage job postings, applications, and communication from one simple, intuitive dashboard.</li>
+            <li>Strengthen Your Employer Brand: Showcase your company culture with rich profile pages, videos, and employee stories to attract the best fits.</li>
           </ul>
-          <ul>
          <div><a href="http://localhost/taskr/company/profile/">Start hiring today</a></div>
    </section>
 </main>
+</div>
