fce1f98fb5849c74d5be9d5cf1fbb6741fb37194
RockyLatchman <r.latchman3624@gmail.com>
2025-10-30 13:45:32 +0700

    CSS for employer registered template

--- templates/employers/onboarding/registered.php
+++ templates/employers/onboarding/registered.php
@@ -1,13 +1,14 @@
 
-<header>
+<header class="top-menu">
     <div class="logotype">
        <h1><a href="/taskr/">Taskr</a></h1>
     </div>
-    <menu><li><a href="/taskr/">Return home</a></li></menu>
+    <menu><li><a href="/taskr/">Return to home</a></li></menu>
 </header>
+<div class="form-container">
 <main>
-    <section>
-       <h1>Registration complete</h1>
+    <section id="registered">
+       <h2>Registration complete</h2>
        <p>
            You are good to go. You can now add listings. If you ever need to
            make account changes you can do it under <span>profile > settings</span>
@@ -15,3 +16,4 @@
        <div><a href="/taskr/employer/dashboard">Go to account</a></div>
     </section>
 </main>
+</div>
